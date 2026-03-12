2d6e228ae0e06c9e6a0e4160095ac19eff00e765
infostreamPB <registration@infostream.cc>
2026-03-12 15:17:05 -0400

    Add files via upload

---
+++ RemoveRMM.ps1
@@ -0,0 +1,23 @@
+﻿wmic product where "name like '%ITSPlatform%'" call uninstall /nointeractive
+
+
+Stop-Service -Name "SAAZappr"
+Stop-Service -Name "SAAZDPMACTL"
+Stop-Service -Name "SAAZRemoteSupport"
+Stop-Service -Name "SAAZScheduler"
+Stop-Service -Name "SAAZServerPlus"
+Stop-Service -Name "SAAZWatchDog"
+If (Test-Path "C:\Program Files (x86)\SAAZOD"){
+   Remove-Item "C:\Program Files (x86)\SAAZOD" -Force -Recurse
+} else {}
+If (Test-Path "C:\Program Files (x86)\SAAZODBKP"){
+   Remove-Item "C:\Program Files (x86)\SAAZODBKP" -Force -Recurse
+} else {}
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest" -Name "ITSPlatformID" -Force
+Remove-Item "HKLM:\SOFTWARE\WOW6432Node\SAAZOD" -Force
+Remove-Item "HKLM:\SYSTEM\CurrentControlSet\Services\SAAZappr" -Force
+Remove-Item "HKLM:\SYSTEM\CurrentControlSet\Services\SAAZDPMACTL" -Force
+Remove-Item "HKLM:\SYSTEM\CurrentControlSet\Services\SAAZRemoteSupport" -Force
+Remove-Item "HKLM:\SYSTEM\CurrentControlSet\Services\SAAZScheduler" -Force
+Remove-Item "HKLM:\SYSTEM\CurrentControlSet\Services\SAAZServerPlus" -Force
+Remove-Item "HKLM:\SYSTEM\CurrentControlSet\Services\SAAZWatchDog" -Force
