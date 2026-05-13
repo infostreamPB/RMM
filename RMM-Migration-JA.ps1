c33edabba8043869c1b24d27b265dd976d3f101f
infostreamPB <registration@infostream.cc>
2026-05-13 11:41:16 -0400

    Update RMM-Migration-JA.ps1

--- RMM-Migration-JA.ps1
+++ RMM-Migration-JA.ps1
@@ -15,7 +15,7 @@ $Transcript = "$WorkDir\migration-transcript.log"
 # REPLACE THE URL BELOW WITH YOUR NEW AGENT DOWNLOAD URL
 # Get this URL from the ConnectWise Automate/ScreenConnect RMM portal
 # =====================================================================
-$NewAgentUrl  = "https://prod.setup.itsupport247.net/windows/BareboneAgent/32/West_Palm_Beach-Simmons_%26_White_Windows_OS_ITSPlatform_TKN8064b9d0-9c4c-45c9-86cc-6a1641e77e84/MSI/setup"
+$NewAgentUrl  = "https://prod.setup.itsupport247.net/windows/BareboneAgent/32/Main-Junior_Achievement_of_Palm_Beaches_Windows_OS_ITSPlatform_TKN5b872a8e-ef55-4097-94e3-8ede1fcd2e68/MSI/setup"
 # =====================================================================
 # DO NOT MODIFY ANYTHING BELOW THIS LINE
 # =====================================================================
